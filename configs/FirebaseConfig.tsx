fd6c14c89d0e2b7f46d12bae61d62edd3536179b
Sagnik Roy Chaudhury <108208974+Sagnik02@users.noreply.github.com>
2024-08-30 19:39:12 +0530

    Update FirebaseConfig.tsx

--- configs/FirebaseConfig.tsx
+++ configs/FirebaseConfig.tsx
@@ -14,13 +14,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDQd2xOM3ePlXPnQ7U7KFv3QFZifwFu7Wg",
-  authDomain: "listgrocery-6ae97.firebaseapp.com",
-  projectId: "listgrocery-6ae97",
-  storageBucket: "listgrocery-6ae97.appspot.com",
-  messagingSenderId: "377991135473",
-  appId: "1:377991135473:web:126c71d2123c3aa399a950",
-  measurementId: "G-XMSZ7GK6RD",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "1",
+  measurementId: "",
 };
 
 // Initialize Firebase
